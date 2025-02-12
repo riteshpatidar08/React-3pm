d43dc719eb30579ce0189bcf154bc161bb30c700
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2025-02-12 16:22:36 +0530

    updated till props

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import { StrictMode ,Component } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+ <App/>
+  </StrictMode>
+);
+
+//JS => javscript
+
+//jsx =>
+
+//Index.html => html runs in the browser
+
+//main.jsx => main file through which all the files are connected with index.html
+
+//App.jsx => root component
+
+//root  => 
+
+//component  =>  class components and functional components ;
+
+// what you want to show on screen
+
+//Button => button ka ui show karna
+//Card => car show krna screen par
+//Drawer => drawer show karna
